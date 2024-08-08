b4fd3082b3c2f4038262949985fa3a4b57ff3720
omardellamazza <omardellamazza@gmail.com>
2024-08-08 15:51:21 +0200

    Added SidebarMenuView

---
+++ EaseGraphs/Views/SidebarMenuView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace EaseGraphs.Views;
+
+public partial class SidebarMenuView : UserControl
+{
+    public SidebarMenuView()
+    {
+        InitializeComponent();
+    }
+}
